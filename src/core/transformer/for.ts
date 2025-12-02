e84f027271a369a732a71eee71ae7d94b1ae2006
nagashimam <masato.ngsm@gmail.com>
2025-12-02 10:47:01 +0900

    fix: Fix mistake in how to get variable name in for block expression

--- src/core/transformer/for.ts
+++ src/core/transformer/for.ts
@@ -1,7 +1,7 @@
 import type {
-	ASTWithSource,
-	PropertyRead,
-	TmplAstForLoopBlock,
+  ASTWithSource,
+  PropertyRead,
+  TmplAstForLoopBlock,
 } from "@angular/compiler";
 import type { TmplAstBranchNodeTransformer } from "../../types/index.js";
 
@@ -15,66 +15,65 @@ import type { TmplAstBranchNodeTransformer } from "../../types/index.js";
  * @returns A 2D array of DOM Nodes representing the transformed for loop.
  */
 export const transformTmplAstForLoopBlock: TmplAstBranchNodeTransformer<
-	TmplAstForLoopBlock
+  TmplAstForLoopBlock
 > = async (
-	forBlock,
-	tmplAstTemplates,
-	properties,
-	transformTmplAstNodes,
-	option,
+  forBlock,
+  tmplAstTemplates,
+  properties,
+  transformTmplAstNodes,
+  option,
 ) => {
-	const result: Node[][] = [];
-	// Handle case for no loop
-	// If variable is checked to have more than 1 items, we don't need case for no loop
-	const name =
-		((forBlock.expression as ASTWithSource)?.ast as PropertyRead)?.name || "";
-	if (!option.nonEmptyItems.includes(name)) {
-		if (forBlock.empty && forBlock.empty.children.length > 0) {
-			for (const parsedEmptyBlock of await transformTmplAstNodes(
-				forBlock.empty.children,
-				tmplAstTemplates,
-				properties,
-				option,
-			)) {
-				result.push(parsedEmptyBlock);
-			}
-		} else {
-			result.push([]);
-		}
-	}
+  const result: Node[][] = [];
+  // Handle case for no loop
+  // If variable is checked to have more than 1 items, we don't need case for no loop
+  const name = forBlock.expression.source || "";
+  if (!option.nonEmptyItems.includes(name)) {
+    if (forBlock.empty && forBlock.empty.children.length > 0) {
+      for (const parsedEmptyBlock of await transformTmplAstNodes(
+        forBlock.empty.children,
+        tmplAstTemplates,
+        properties,
+        option,
+      )) {
+        result.push(parsedEmptyBlock);
+      }
+    } else {
+      result.push([]);
+    }
+  }
 
-	// Handle case for loop once
-	// Set $index variable in for loop
-	properties.set("$index", "0");
-	for (const parsedForBlock of await transformTmplAstNodes(
-		forBlock.children,
-		tmplAstTemplates,
-		properties,
-		option,
-	)) {
-		result.push(parsedForBlock);
-	}
+  // Handle case for loop once
+  // Set $index variable in for loop
+  properties.set("$index", "0");
+  for (const parsedForBlock of await transformTmplAstNodes(
+    forBlock.children,
+    tmplAstTemplates,
+    properties,
+    option,
+  )) {
+    result.push(parsedForBlock);
+  }
 
-	// Handle case for loop twice
-	const firstLoop = await transformTmplAstNodes(
-		forBlock.children,
-		tmplAstTemplates,
-		properties,
-		option,
-	);
-	properties.delete("$index");
-	properties.set("$index", "1");
-	const secondLoop = await transformTmplAstNodes(
-		forBlock.children,
-		tmplAstTemplates,
-		properties,
-		option,
-	);
+  // Handle case for loop twice
+  const firstLoop = await transformTmplAstNodes(
+    forBlock.children,
+    tmplAstTemplates,
+    properties,
+    option,
+  );
+  properties.delete("$index");
+  properties.set("$index", "1");
+  const secondLoop = await transformTmplAstNodes(
+    forBlock.children,
+    tmplAstTemplates,
+    properties,
+    option,
+  );
 
-	for (let i = 0; i < firstLoop.length; i++) {
-		firstLoop[i].push(...secondLoop[i]);
-		result.push(firstLoop[i]);
-	}
+  for (let i = 0; i < firstLoop.length; i++) {
+    firstLoop[i].push(...secondLoop[i]);
+    result.push(firstLoop[i]);
+  }
 
-	return result;
+  return result;
 };
